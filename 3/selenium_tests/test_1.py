aa79215252cf3bc3bcfc5a2a39d76da8a57d2d62
s20500-pj <s20500@pjwstk.edu.pl>
2022-11-20 16:39:21 +0100

    dodanie dwóch testów selenium

---
+++ 3/selenium_tests/test_1.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+import logging
+
+from selenium.webdriver.common.by import By
+
+logger = logging.getLogger('przypinki_test')
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+"""
+Należy podać ścieżkę do drivera
+"""
+driver = webdriver.Chrome(executable_path=r"D:\programowanie\INNE\chromedriver.exe")
+logger.info('Przechodzę na stronę amazon.pl')
+driver.get('https://przypinki.pl/')
+cookies = driver.find_element(By.ID, "accept-cookies-checkbox")
+logger.info('Akceptuję ciasteczka')
+cookies.click()
+logger.info('Klikam w kategorie Studniówka')
+driver.find_element(By.XPATH, "/html/body/div/aside/nav/ul/li[1]/a").click()
+logger.info('Przechodzę na drugą stronę')
+driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[2]/a[1]").click()
+logger.info('Klikam na pierwszą przypinkę')
+driver.find_element(By.XPATH, "/html/body/div/div/div[2]/a[1]").click()
+logger.info('Dodaje do koszyka 1 sztukę')
+driver.find_element(By.ID, "buttonDoKoszyka").click()
+
+try:
+    logger.info('Sprawdzam czy w koszyku jest 1 sztuka')
+    driver.find_element(By.ID, "koszykNaglowek")
+except NoSuchElementException:
+    logger.error("Test failed.")
+driver.close()
---
+++ 3/selenium_tests/test_2.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+import logging
+
+from selenium.webdriver.common.by import By
+
+logger = logging.getLogger('wikipedia_test')
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+"""
+Należy podać ścieżkę do drivera
+Kilkukrotne odpalenie testu nie jest możliwe gdyż wikipedia wprowadzi blokadę captcha
+"""
+driver = webdriver.Chrome(executable_path=r"D:\programowanie\INNE\chromedriver.exe")
+logger.info('Przechodzę na stronę wikipedia.pl')
+driver.get('https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna')
+logger.info('Klikam zaloguj się')
+driver.find_element(By.XPATH, "/html/body/div[4]/div[1]/nav/div/ul/li[5]/a").click()
+logger.info('Klikam zaloguj się')
+driver.find_element(By.XPATH, "/html/body/div[4]/div[1]/nav/div/ul/li[5]/a").click()
+logger.info('Wprowadzam dane użytkownika')
+driver.find_element(By.ID, "wpName1").send_keys("testowe")
+driver.find_element(By.ID, "wpPassword1").send_keys("password")
+logger.info('Klikam zaloguj się')
+driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div[4]/div[1]/div[3]/form/div/div[4]/div/button").click()
+try:
+    message = driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div[4]/div[1]/div[3]/form/div[1]")
+    desired_message = "Podany login lub hasło są nieprawidłowe. Spróbuj jeszcze raz."
+    logger.info(message.text)
+    if (desired_message == message.text):
+        logger.info("test succeded")
+except NoSuchElementException:
+    logger.error("Test failed.")
+driver.close()
